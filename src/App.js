846e0b3635b1f7b94955ceb1cb523047b22c3fb7
LukitaChan <kamery_94@hotmail.com>
2022-09-02 16:11:26 -0500

    repo gh-pages

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+//import './App.css';
+import React from "react";
+// import ContactForm from "./components/ContactForm";
+import SongSearch from "./components/SongSearch";
+
+function App() {
+  return (
+    <>
+      <h1>Buscador De Canciones</h1>
+      <hr />
+      <SongSearch />
+    </>
+  );
+}
+
+export default App;
